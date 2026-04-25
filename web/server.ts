ff7e394201d08d174894bf024261d90723d08860
John Castillo <johnacastillo@sonic.net>
2026-04-25 15:50:21 -0700

    Use WEB_PORT env var

--- web/server.ts
+++ web/server.ts
@@ -5,7 +5,7 @@ import { Server as SocketIOServer } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = '0.0.0.0';
-const port = parseInt(process.env.PORT || process.env.RAILWAY_TCP_APPLICATION_PORT || '3000', 10);
+const port = parseInt(process.env.PORT || process.env.RAILWAY_TCP_APPLICATION_PORT || process.env.WEB_PORT || '8080', 10);
 
 const app = createNext({ dev, hostname, port });
 const handle = app.getRequestHandler();
